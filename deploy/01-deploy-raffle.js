dd480855b1dc2a226aa8b1a282ee7a994e5800ee
Fiat Bruno <fiatbruno10@gmail.com>
2023-01-09 20:50:25 +0200

    Added all constructor arguments from deploy script

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,17 +1,32 @@
 const { network, ethers } = require("hardhat")
-const { developmentChains } = require("../helper-hardhat-config")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+
+const SUB_ID_FUND_AMOUNT = ethers.utils.parseEther("2")
 
 module.exports = async function (getNamedAccounts, deployments) {
     const { deploy, log } = deployments
     const { deployer } = getNamedAccounts()
-    let vrfCoordinatorV2Address
+    const chainId = network.config.chainId
+    let vrfCoordinatorV2Address, subcriptionId
 
     if (developmentChains.includes(network.name)) {
-        const VRFCoordinatorV2Mock = await ethers.getContractAt("VRFCoordinatorV2Mock")
-        vrfCoordinatorV2Address = VRFCoordinatorV2Mock.address
+        const vrfCoordinatorV2Mock = await ethers.getContractAt("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const transactionResponse = await vrfCoordinatorV2Mock.createSubcription()
+        const transactionReceipt = await transactionResponse.wait(1)
+        subcriptionId = transactionReceipt.events[0].args.subId
+        await vrfCoordinatorV2Mock.fundSubscription(subcriptionId, SUB_ID_FUND_AMOUNT)
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+        subcriptionId = networkConfig[chainId]["subscriptionId"]
     }
 
-    const args = []
+    const entranceFee = networkConfig[chainId]["entranceFee"]
+    const gasLane = networkConfig[chainId]["gasLane"]
+    const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
+    const interval = networkConfig[chainId]["interval"]
+
+    const args = [vrfCoordinatorV2Address, entranceFee, gasLane, subcriptionId, callbackGasLimit, interval]
     const raffle = await deploy("Raffle", {
         from: deployer,
         args: [],
